fe474de8d558757d20bd208cf553ccee8b1330ba
crjcarlos1 <crjcarlos1@users.noreply.github.com>
2020-06-09 12:08:08 -0500

    Added main_menu.xml Udated MainActivity.java Updated BaseActivity.java

--- app/src/main/java/com/cralos/mydaggerapp/BaseActivity.java
+++ app/src/main/java/com/cralos/mydaggerapp/BaseActivity.java
@@ -2,9 +2,7 @@ package com.cralos.mydaggerapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.util.Log;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.lifecycle.Observer;
@@ -21,15 +19,15 @@ public abstract class BaseActivity extends DaggerAppCompatActivity {
     private static final String TAG = "BaseActivity";
 
     @Inject
-    SessionManager sessionManager;
+    public SessionManager sessionManager;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         subscribeObservers();
     }
 
-    private void subscribeObservers(){
+    private void subscribeObservers() {
         sessionManager.getAuthUser().observe(this, new Observer<AuthResource<User>>() {
             @Override
             public void onChanged(AuthResource<User> userAuthResource) {
@@ -55,8 +53,8 @@ public abstract class BaseActivity extends DaggerAppCompatActivity {
         });
     }
 
-    private void navLoginScreen(){
-        Intent intent=new Intent(this, AuthActivity.class);
+    private void navLoginScreen() {
+        Intent intent = new Intent(this, AuthActivity.class);
         startActivity(intent);
     }
 
--- app/src/main/java/com/cralos/mydaggerapp/ui/main/MainActivity.java
+++ app/src/main/java/com/cralos/mydaggerapp/ui/main/MainActivity.java
@@ -1,8 +1,12 @@
 package com.cralos.mydaggerapp.ui.main;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.cralos.mydaggerapp.BaseActivity;
@@ -13,10 +17,27 @@ public class MainActivity extends BaseActivity {
     private static final String TAG = "MainActivity";
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(getApplicationContext(), "MainActivity", Toast.LENGTH_SHORT).show();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout: {
+                sessionManager.logOut();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
