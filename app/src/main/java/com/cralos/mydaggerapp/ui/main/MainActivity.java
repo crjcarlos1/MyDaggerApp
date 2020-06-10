2c0f165979e8175aa35a7b78cc71d62c429a7e8b
crjcarlos1 <crjcarlos1@users.noreply.github.com>
2020-06-10 17:19:12 -0500

    Added drawer_view.xml Updated activity_main.xml Updated MainActivity.java

--- app/src/main/java/com/cralos/mydaggerapp/ui/main/MainActivity.java
+++ app/src/main/java/com/cralos/mydaggerapp/ui/main/MainActivity.java
@@ -7,22 +7,28 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.cralos.mydaggerapp.BaseActivity;
 import com.cralos.mydaggerapp.R;
 import com.cralos.mydaggerapp.ui.main.posts.PostsFragment;
-import com.cralos.mydaggerapp.ui.main.profile.ProfileFragment;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "MainActivity";
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
         testFragment();
     }
 
@@ -42,6 +48,21 @@ public class MainActivity extends BaseActivity {
             }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_posts:{
+                break;
+            }
+            case R.id.nav_profile:{
+                break;
+            }
+        }
+        item.setChecked(true);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     private void testFragment() {
