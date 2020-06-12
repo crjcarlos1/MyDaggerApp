0a3c68752b2d73fd99d86045e767cc5bb37c649f
crjcarlos1 <crjcarlos1@users.noreply.github.com>
2020-06-12 12:43:43 -0500

    Added AuthScope.java Added MainScope.java Updated ActivityBuildersModule.java Updated AuthModule.java Updated MainModule.java

--- app/src/main/java/com/cralos/mydaggerapp/di/ActivityBuildersModule.java
+++ app/src/main/java/com/cralos/mydaggerapp/di/ActivityBuildersModule.java
@@ -1,9 +1,11 @@
 package com.cralos.mydaggerapp.di;
 
 import com.cralos.mydaggerapp.di.auth.AuthModule;
+import com.cralos.mydaggerapp.di.auth.AuthScope;
 import com.cralos.mydaggerapp.di.auth.AuthViewModelsModule;
 import com.cralos.mydaggerapp.di.main.MainFragmentBuildersModule;
 import com.cralos.mydaggerapp.di.main.MainModule;
+import com.cralos.mydaggerapp.di.main.MainScope;
 import com.cralos.mydaggerapp.di.main.MainViewModelsModule;
 import com.cralos.mydaggerapp.ui.auth.AuthActivity;
 import com.cralos.mydaggerapp.ui.main.MainActivity;
@@ -18,17 +20,14 @@ import dagger.android.ContributesAndroidInjector;
 @Module
 public abstract class ActivityBuildersModule {      //usamos abstract cada que utilicemos la notación @ContributesAndroidInjector
 
-    @ContributesAndroidInjector(
-            modules = {AuthViewModelsModule.class, AuthModule.class}
-    )
+    @AuthScope
+    @ContributesAndroidInjector(modules = {AuthViewModelsModule.class, AuthModule.class})
     abstract AuthActivity contributeAuthActivity();
 
+    @MainScope
     @ContributesAndroidInjector(
             modules = {                                 //le decimos que 'MainFragmentBuildersModule' es un subcomponente y solo vive en mainActivity
-                    MainFragmentBuildersModule.class,
-                    MainViewModelsModule.class,
-                    MainModule.class}
-    )
+                    MainFragmentBuildersModule.class, MainViewModelsModule.class, MainModule.class})
     abstract MainActivity contributeMainActivity();
 
 }
--- app/src/main/java/com/cralos/mydaggerapp/di/main/MainModule.java
+++ app/src/main/java/com/cralos/mydaggerapp/di/main/MainModule.java
@@ -10,11 +10,13 @@ import retrofit2.Retrofit;
 @Module
 public class MainModule {
 
+    @MainScope
     @Provides
     public static PostsRecyclerAdapter providesAdapter() {
         return new PostsRecyclerAdapter();
     }
 
+    @MainScope
     @Provides
     public static MainApi provideMainApi(Retrofit retrofit) {
         return retrofit.create(MainApi.class);
